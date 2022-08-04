4d0734e1677ce297e0e0bf0df7ae11332c4fe94c
CheesyPhoenix <sindre.he.moe@gmail.com>
2022-08-04 14:09:58 +0200

    added pwa support

--- BetterSchool-Frontend/vite.config.ts
+++ BetterSchool-Frontend/vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+	plugins: [
+		svelte(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				background_color: "#333",
+				theme_color: "#333",
+				name: "BetterSchool",
+				short_name: "BetterSchool",
+				start_url: "/",
+				display: "standalone",
+				icons: [
+					{
+						src: "/betterschool-icon-512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "/betterschool-icon-192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/betterschool-icon-192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+			workbox: {
+				runtimeCaching: [
+					{
+						handler: "NetworkFirst",
+						urlPattern: "https://api.betterschool.cheesyphoenix.tk",
+						method: "GET",
+					},
+				],
+			},
+		}),
+	],
+});
