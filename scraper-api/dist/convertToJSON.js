bb66174a21cc17af0a198f9a3073abc33bf8d097
CheesyPhoenix <sindre.he.moe@gmail.com>
2022-09-11 15:05:54 +0200

    JSON list of all schools using visma

--- scraper-api/dist/convertToJSON.js
+++ scraper-api/dist/convertToJSON.js
@@ -4,5 +4,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-const list = fs_1.default.readFileSync("./allSchools.txt");
-console.log(list.toString().split("\r\n"));
+const list = fs_1.default.readFileSync("./allSchools.txt").toString().split("\r\n");
+let namedList = [];
+list.forEach((e) => {
+    namedList.push({
+        name: capitalizeBySpace(e
+            .replace("https://", "")
+            .replace(".inschool.visma.no/", "")
+            .replace("-", " ")).replace("Vgs", "VGS"),
+        url: e,
+    });
+});
+function capitalizeBySpace(string) {
+    return string
+        .split(" ")
+        .map((e) => {
+        if (e.length == 0)
+            return e;
+        return e[0].toUpperCase() + (e.length > 1 ? e.slice(1) : "");
+    })
+        .join(" ");
+}
+fs_1.default.writeFileSync("./allSchools.json", JSON.stringify(namedList));
