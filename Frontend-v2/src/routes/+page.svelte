cf3996bc97693833035915e89d934ec1fb062f17
CheesyPhoenix <sindre.he.moe@gmail.com>
2023-02-12 17:14:18 +0100

    docker setup

--- Frontend-v2/src/routes/+page.svelte
+++ Frontend-v2/src/routes/+page.svelte
@@ -8,16 +8,53 @@
 </svelte:head>
 
 <main in:fly={{ y: 100 }} out:fly={{ y: -100 }} class="absolute w-screen">
-	<h1 class="text-3xl font-bold m-auto w-max mt-12">BetterSchool</h1>
-	<h3 class="w-max m-auto">The better alternative to Visma Inschool</h3>
+	<div class="m-2 overflow-y-auto max-h-screen">
+		<h1 class="text-3xl font-bold m-auto max-w-[max-content] mt-12">
+			BetterSchool
+		</h1>
+		<h3 class="max-w-[max-content] m-auto">
+			The better alternative to Visma Inschool
+		</h3>
 
-	<br />
+		<br />
 
-	<img src={BSLogo} alt="BetterSchool Logo" class="w-40 m-auto mb-4" />
+		<div class="max-w-md m-auto flex">
+			<p class="inline-block">
+				BetterSchool is a better way of viewing your Visma InSchool
+				schedule. Don't waste time logging in to the app every time you
+				just want to check when your next class starts, use BetterSchool
+				instead!
+			</p>
+		</div>
 
-	<a
-		href="/schools"
-		class="w-max m-auto block bg-zinc-500 p-2 rounded-xl hover:bg-zinc-400 hover:rounded-lg active:bg-zinc-600 active:top-4 duration-150"
-		>Get started!</a
-	>
+		<br />
+
+		<a href="/schools" class="btn max-w-md m-auto">Get started!</a>
+
+		<br />
+		<br />
+		<br />
+
+		<h2 class="m-auto max-w-md text-xl font-bold mb-2">
+			Are you a developer?
+		</h2>
+		<p class="m-auto max-w-md">
+			This is an open-source project on <a
+				href="https://github.com/CheesyPhoenix/BetterSchool"
+				class="underline">GitHub</a
+			>, feel free to contribute, fork or open an issue. All help is
+			greatly appreciated.
+
+			<br />
+			<br />
+
+			You can even self-host your own instance of both the API and
+			frontend using docker. Check out the
+			<a
+				href="https://github.com/CheesyPhoenix/BetterSchool"
+				class="underline">GitHub page</a
+			>
+			for more information.
+		</p>
+	</div>
 </main>
